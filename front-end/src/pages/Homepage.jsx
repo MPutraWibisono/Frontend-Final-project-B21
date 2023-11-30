7499f540b33e272cc5ef2b96b15175bfdde49d13
Muhammad Putra Wibisono <mputrawibisone993@gmail.com>
2023-11-30 08:24:42 +0800

    homepage:3/4

--- front-end/src/pages/Homepage.jsx
+++ front-end/src/pages/Homepage.jsx
@@ -37,11 +37,16 @@ const Homepage = () => {
       <section>
         <div className=" m-auto ">
           <div className="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-12">
-            <div className=" col-span-4 lg:col-span-7">
-              <img src="../../public/belajar.jpg" alt="belajar-kelompok" />
+            <div
+              style={{
+                backgroundImage: "url(../../public/belajar.jpg)",
+              }}
+              className="relative bg-cover bg-center no-repeat col-span-4 lg:col-span-7"
+            >
+              <div className="absolute inset-0 bg-gradient-to-b from-white from-5% via-lime-100/10 via-50% to-green-300 to-95%"></div>
             </div>
-            <div className="col-span-4 lg:col-span-5 bg-gradient-to-b from-white from-5% via-lime-100 via-50% to-green-300 to-80% flex justify-center items-center flex-col ">
-              <p className="font-sans">Belajar dari Praktisi Terbaik</p>
+            <div className="h-96 text-xl col-span-4 lg:col-span-5 bg-gradient-to-b from-white from-5% via-lime-100 via-50% to-green-300 to-95% flex justify-center items-center flex-col ">
+              <h1 className="pb-4">Belajar dari Praktisi Terbaik</h1>
               {/*<!-- Component: Large primary basic button --> */}
               <button className="inline-flex items-center justify-center h-12 gap-2 px-6 text-sm font-medium tracking-wide text-white transition duration-300 rounded-[20px] whitespace-nowrap bg-emerald-500 hover:bg-emerald-600 focus:bg-emerald-700 focus-visible:outline-none disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none">
                 <span>Ikuti Kelas</span>
@@ -53,65 +58,19 @@ const Homepage = () => {
       {/*<!-- End Two columns even layout --> */}
 
       {/*<!-- Component: Carousel with indicators & controls inside --> */}
-      <div className="glide-03 relative w-full">
-        <div className="flex">
-          <span>Kategori Belajar</span>
-          <span className="ms-auto text-sky-600">Lihat semua</span>
+      <div className="bg-gradient-to-b from-green-300 from-50% via-lime-100 via-70% to-white to-95% glide-03 relative w-full">
+        <div className="py-5 px-2 flex">
+          <h2>Kategori Belajar</h2>
+          <a href="" className="ms-auto text-sky-600">
+            Lihat semua
+          </a>
         </div>
         {/*    <!-- Slides --> */}
         <div className="overflow-hidden" data-glide-el="track">
-          <ul className="whitespace-no-wrap flex-no-wrap [backface-visibility: hidden] [transform-style: preserve-3d] [touch-action: pan-Y] [will-change: transform] relative flex w-full overflow-hidden p-0">
+          <ul className="whitespace-no-wrap  flex-no-wrap [backface-visibility: hidden] [transform-style: preserve-3d] [touch-action: pan-Y] [will-change: transform] relative flex w-full overflow-hidden p-0">
             <li>
               <a href="#">
-                <div className="h-2/3 lg:max-h-80 overflow-hidden">
-                  <img
-                    src="../../public/android dev.jpg"
-                    alt="Android Dev"
-                    className="h-full w-full object-none object-center"
-                  />
-                </div>
-                <p className="text-center">Android Dev</p>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <div className="h-2/3 lg:max-h-80 overflow-hidden">
-                  <img
-                    src="../../public/android dev.jpg"
-                    alt="Android Dev"
-                    className="h-full w-full object-none object-center"
-                  />
-                </div>
-                <p className="text-center">Android Dev</p>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <div className="h-2/3 lg:max-h-80 overflow-hidden">
-                  <img
-                    src="../../public/android dev.jpg"
-                    alt="Android Dev"
-                    className="h-full w-full object-none object-center"
-                  />
-                </div>
-                <p className="text-center">Android Dev</p>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <div className="h-2/3 lg:max-h-80 overflow-hidden">
-                  <img
-                    src="../../public/android dev.jpg"
-                    alt="Android Dev"
-                    className="h-full w-full object-none object-center"
-                  />
-                </div>
-                <p className="text-center">Android Dev</p>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <div className="h-2/3 lg:max-h-80 overflow-hidden">
+                <div className="h-2/3 lg:max-h-80 overflow-hidden rounded-[13px]">
                   <img
                     src="../../public/android dev.jpg"
                     alt="Android Dev"
@@ -173,25 +132,29 @@ const Homepage = () => {
         </div>
       </div>
       {/*<!-- End Carousel with indicators & controls inside --> */}
-      <div className="flex">
-        <span>Kursus Populer</span>
-        <span className="ms-auto text-sky-600">Lihat semua</span>
+      <div className=" px-2 flex">
+        <h2 className="pb-4"> Kursus Populer</h2>
+        <a href="" className="ms-auto text-sky-600">
+          Lihat semua
+        </a>
       </div>
 
-      {/*<!-- Component: Small primary basic button --> */}
-      <button className="inline-flex items-center justify-center h-8 gap-2 px-4 text-xs font-medium tracking-wide text-white transition duration-300 rounded-full focus-visible:outline-none whitespace-nowrap bg-emerald-500 hover:bg-emerald-600 focus:bg-emerald-700 disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none">
-        <span>All</span>
-      </button>
-      <button className="inline-flex items-center justify-center h-8 gap-2 px-4 text-xs font-medium tracking-wide text-white transition duration-300 rounded-full focus-visible:outline-none whitespace-nowrap bg-emerald-500 hover:bg-emerald-600 focus:bg-emerald-700 disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none">
-        <span>UI/UX</span>
-      </button>
-      <button className="inline-flex items-center justify-center h-8 gap-2 px-4 text-xs font-medium tracking-wide text-white transition duration-300 rounded-full focus-visible:outline-none whitespace-nowrap bg-emerald-500 hover:bg-emerald-600 focus:bg-emerald-700 disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none">
-        <span>IOS Development</span>
-      </button>
-      <button className="inline-flex items-center justify-center h-8 gap-2 px-4 text-xs font-medium tracking-wide text-white transition duration-300 rounded-full focus-visible:outline-none whitespace-nowrap bg-emerald-500 hover:bg-emerald-600 focus:bg-emerald-700 disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none">
-        <span>Data Science</span>
-      </button>
-      {/*<!-- End Small primary basic button --> */}
+      <div className="px-5 flex space-x-3">
+        {/*<!-- Component: Small primary basic button --> */}
+        <button className="inline-flex items-center justify-center h-8 gap-2 px-4 text-xs font-medium tracking-wide text-white transition duration-300 rounded-full focus-visible:outline-none whitespace-nowrap bg-emerald-500 hover:bg-emerald-600 focus:bg-emerald-700 disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none">
+          <span>All</span>
+        </button>
+        <button className="inline-flex items-center justify-center h-8 gap-2 px-4 text-xs font-medium tracking-wide text-white transition duration-300 rounded-full focus-visible:outline-none whitespace-nowrap bg-emerald-500 hover:bg-emerald-600 focus:bg-emerald-700 disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none">
+          <span>UI/UX</span>
+        </button>
+        <button className="inline-flex items-center justify-center h-8 gap-2 px-4 text-xs font-medium tracking-wide text-white transition duration-300 rounded-full focus-visible:outline-none whitespace-nowrap bg-emerald-500 hover:bg-emerald-600 focus:bg-emerald-700 disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none">
+          <span>IOS Development</span>
+        </button>
+        <button className="inline-flex items-center justify-center h-8 gap-2 px-4 text-xs font-medium tracking-wide text-white transition duration-300 rounded-full focus-visible:outline-none whitespace-nowrap bg-emerald-500 hover:bg-emerald-600 focus:bg-emerald-700 disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none">
+          <span>Data Science</span>
+        </button>
+        {/*<!-- End Small primary basic button --> */}
+      </div>
 
       <div className="flex justify-center items-center flex-wrap p-2 gap-5">
         <CourseCard />
