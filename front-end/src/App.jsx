6b033d003c5bd1767990f1b41620e24c2b699aa4
Alvan MAzid <alvantwent@gmail.com>
2023-12-01 10:39:05 +0700

    konflik

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/NavbarComponent";
+import NavbarComponent from "./components/NavbarComponent";
+import Header from "./components/Header";
+import Homepage from "./pages/Homepage";
 import Kelas from "./pages/Kelas";
 import KelasPremium from "./pages/KelasPremium";
 import KelasGratis from "./pages/KelasGratis";
@@ -8,7 +10,9 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
+      <NavbarComponent />
       <Routes>
+      <Route path="/" element={<Homepage />} />
         <Route path="/" element={<Kelas />} />
         <Route path="/kelasPremium" element={<KelasPremium />} />
         <Route path="/kelasGratis" element={<KelasGratis />} />
