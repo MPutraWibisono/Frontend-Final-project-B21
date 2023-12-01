d10bab21b6bb56d9da1f465c89e61145b7e091de
Alvan Mazid <113406830+alvannnnn@users.noreply.github.com>
2023-12-01 10:41:36 +0700

    Alvan (#8)
    * first commit alvan
    
    * add package and modify
    
    * add new pages
    
    * add logo
    
    * add logo
    
    * modify
    
    * rename NavbarComponent to Header
    
    * modified
    
    * modified
    
    * modified
    
    * modified
    
    * modified
    
    * konflik
    
    ---------
    
    Co-authored-by: GilangAlfarizi <89723613+GilangAlfarizi@users.noreply.github.com>

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,13 +1,22 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import NavbarComponent from "./components/NavbarComponent";
 import Header from "./components/Header";
 import Homepage from "./pages/Homepage";
+import Kelas from "./pages/Kelas";
+import KelasPremium from "./pages/KelasPremium";
+import KelasGratis from "./pages/KelasGratis";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <NavbarComponent />
       <Routes>
-        <Route path="/" element={<Homepage />} />
+      <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<Kelas />} />
+        <Route path="/kelasPremium" element={<KelasPremium />} />
+        <Route path="/kelasGratis" element={<KelasGratis />} />
       </Routes>
     </BrowserRouter>
   );
