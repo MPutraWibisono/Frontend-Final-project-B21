19f89684b1c2ea037d746518666d9a3e4127930a
Alvan MAzid <alvantwent@gmail.com>
2023-12-13 10:50:42 +0700

    update

--- front-end/src/pages/ChangePass.jsx
+++ front-end/src/pages/ChangePass.jsx
@@ -76,140 +76,145 @@ const ChangePass = () => {
 
   return (
     <>
-      <Disclosure className="bg-cream06 h-20">
-        <div className="flex items-center">
-          <IoArrowBackSharp className="h-6 w-6 text-black mt-1" />
-          <div className="text-1xl text-black mt-1 mr-2">
+      {/* Header */}
+      <Disclosure className="bg-paleOrange h-20">
+        <div className="flex items-center w-full">
+          <IoArrowBackSharp className="h-6 w-6 text-pinkTone mt-1" />
+          <div className="text-1xl text-pinkTone mt-1 ml-2">
             <Link to="/">Kembali ke Beranda</Link>
           </div>
         </div>
       </Disclosure>
-      <div className="bg-pink01 shadow">
-        <div className="text-center mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <h1 className="text-2xl tracking-tight text-white">Akun</h1>
-        </div>
-      </div>
 
-      <main className="flex items-center justify-center">
-        {/* Sidebar */}
-        <div className="drawer lg:drawer-open">
-          <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-          <div className="drawer-content flex flex-col items-center justify-center">
-            {/* Page content here */}
+      {/* Main Container */}
+      <div className="flex justify-center items-center h-screen">
+        <div className="relative bg-white rounded-lg overflow-hidden shadow-md flex flex-col w-3/4 border border-pinkTone">
+          <div className="bg-pinkTone text-white p-4 flex items-center justify-center rounded-t-lg">
+            <h1 className="text-2xl tracking-tight">Akun</h1>
           </div>
-          <div className="drawer-side">
-            <label
-              htmlFor="my-drawer-2"
-              aria-label="close sidebar"
-              className="drawer-overlay"
-            ></label>
-            <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content my-ul">
-              {/* Sidebar content here */}
-              <li className="mt-5">
+          <div className="flex">
+            <ul className="col-span-1 p-4">
+              {/* Konten Sidebar */}
+              <li
+                style={{ marginTop: "2rem" }}
+                className="text-1xl flex items-center justify-between"
+              >
                 <Link to="/profile">
-                  <IoPencilSharp className="text-brown01" />
-                  Profil Saya
+                  <div className="flex items-center">
+                    <IoPencilSharp className="text-pinkTone mr-2" />
+                    <span>Profil Saya</span>
+                  </div>
                 </Link>
               </li>
-              <li className="mt-5">
+              <li
+                style={{ marginTop: "2rem" }}
+                className="text-1xl flex items-center justify-between"
+              >
                 <Link to="/changepassword">
-                  <IoSettingsOutline className="text-brown01" />
-                  Ubah Password
+                  <div className="flex items-center">
+                    <IoSettingsOutline className="text-pinkTone mr-2" />
+                    <span>Ubah Password</span>
+                  </div>
                 </Link>
               </li>
-              <li className="mt-5">
+              <li
+                style={{ marginTop: "2rem" }}
+                className="text-1xl flex items-center justify-between"
+              >
                 <Link to="/purchasehistory">
-                  <IoCartOutline className="text-brown01" />
-                  Riwayat Pembayaran
+                  <div className="flex items-center">
+                    <IoCartOutline className="text-pinkTone mr-2" />
+                    <span>Riwayat Pembayaran</span>
+                  </div>
                 </Link>
               </li>
-              <li className="mt-5">
+              <li
+                style={{ marginTop: "2rem" }}
+                className="text-1xl flex items-center justify-between"
+              >
                 <Link to="/logout">
-                  <IoLogOutOutline className="text-brown01" />
-                  Keluar
+                  <div className="flex items-center">
+                    <IoLogOutOutline className="text-pinkTone mr-2" />
+                    <span>Keluar</span>
+                  </div>
                 </Link>
               </li>
             </ul>
           </div>
-        </div>
 
-        {/* Content */}
-        <div className="container mx-auto">
-          <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-            <div className="text-center">
-              <div className="text-2xl">Ubah Password</div>
-            </div>
-            <div className="mt-2 relative block mb-4 lg:mb-8">
-              <br />
-              <p className="float-left">Masukkan Password Lama</p>
-
-              <br />
-            </div>
-            <div className="mt-2 relative block mb-4 lg:mb-8">
-              <br />
-              <p className="float-left">Masukkan Password Baru</p>
-
-              <br />
-              <input
-                type={newPassValue.showPass ? "text" : "password"}
-                name="password"
-                id="passInput2"
-                placeholder="Password Baru"
-                className="float-left border-2 rounded-2xl w-full p-2 text-black"
-                value={newPassValue.password}
-                onChange={handlePassBaru}
-                required
-              />
-
-              <button
-                className="absolute right-4 top-14"
-                onClick={toggleVisibility2}
-              >
-                {!newPassValue.showPass ? (
-                  <PiEye color="grey" size={30} />
-                ) : (
-                  <PiEyeSlash color="grey" size={30} />
-                )}
-              </button>
-            </div>
-            <div className="mt-2 relative block mb-4 lg:mb-8">
-              <br />
-              <p className="float-left">Ulangi Password Baru</p>
-
-              <br />
-              <input
-                type={passValue.showPass ? "text" : "password"}
-                name="password"
-                id="passInput3"
-                placeholder="Ulangi Password"
-                className="float-left border-2 rounded-2xl w-full p-2 text-black"
-                value={passValue.password}
-                onChange={handleUlangiPass}
-                required
-              />
-
-              <button
-                className="absolute right-4 top-14"
-                onClick={toggleVisibility3}
-              >
-                {!passValue.showPass ? (
-                  <PiEye color="grey" size={30} />
-                ) : (
-                  <PiEyeSlash color="grey" size={30} />
-                )}
-              </button>
-            </div>
-            <div className="text-center">
-              <button
-                onClick={validasi}
-                className="text-sm rounded-3xl font-semibold leading-6 bg-brown01 text-white border-4 border-brown01 m-10"
-              >
-                Ubah Password
-              </button>
+          {/* Content */}
+          <div className="col-span-3 p-4 flex flex-col items-center">
+            <div className="container mx-auto">
+              <div className="text-center">
+                <div className="text-2xl">Ubah Password</div>
+              </div>
+              <div className="mt-2 relative block mb-4 lg:mb-8">
+                <br />
+                <p className="float-left">Masukkan Password Lama</p>
+                <br />
+              </div>
+              <div className="mt-2 relative block mb-4 lg:mb-8">
+                <br />
+                <p className="float-left">Masukkan Password Baru</p>
+                <br />
+                <input
+                  type={newPassValue.showPass ? "text" : "password"}
+                  name="password"
+                  id="passInput2"
+                  placeholder="Password Baru"
+                  className="float-left border-2 rounded-2xl w-full p-2 text-black"
+                  value={newPassValue.password}
+                  onChange={handlePassBaru}
+                  required
+                />
+                <button
+                  className="absolute right-4 top-14"
+                  onClick={toggleVisibility2}
+                >
+                  {!newPassValue.showPass ? (
+                    <PiEye color="grey" size={30} />
+                  ) : (
+                    <PiEyeSlash color="grey" size={30} />
+                  )}
+                </button>
+              </div>
+              <div className="mt-2 relative block mb-4 lg:mb-8">
+                <br />
+                <p className="float-left">Ulangi Password Baru</p>
+                <br />
+                <input
+                  type={passValue.showPass ? "text" : "password"}
+                  name="password"
+                  id="passInput3"
+                  placeholder="Ulangi Password"
+                  className="float-left border-2 rounded-2xl w-full p-2 text-black"
+                  value={passValue.password}
+                  onChange={handleUlangiPass}
+                  required
+                />
+                <button
+                  className="absolute right-4 top-14"
+                  onClick={toggleVisibility3}
+                >
+                  {!passValue.showPass ? (
+                    <PiEye color="grey" size={30} />
+                  ) : (
+                    <PiEyeSlash color="grey" size={30} />
+                  )}
+                </button>
+              </div>
+              <div className="text-center">
+                <button
+                  onClick={validasi}
+                  className="text-sm rounded-3xl font-semibold leading-6 bg-darkRed text-white border-4 border-darkRed m-10"
+                >
+                  Ubah Password
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </main>
+      </div>
     </>
   );
 };
--- front-end/src/pages/MyProfile.jsx
+++ front-end/src/pages/MyProfile.jsx
@@ -27,123 +27,132 @@ function InputForm({ label, id, type, placeholder }) {
     </div>
   );
 }
-const MyProfile = () => {
+
+const Account = () => {
   return (
     <>
-      <Disclosure className="bg-cream06 h-20">
-        <div className="flex items-center">
+      {/* Header */}
+      <Disclosure className="bg-paleOrange h-20">
+        <div className="flex items-center w-full">
           <IoArrowBackSharp className="h-6 w-6 text-pinkTone mt-1" />
-          <div className="text-1xl text-pinkTone mt-1 mr-2">
+          <div className="text-1xl text-pinkTone mt-1 ml-2">
             <Link to="/">Kembali ke Beranda</Link>
           </div>
         </div>
       </Disclosure>
-      <div className="bg-pinkTone shadow">
-        <div className="text-center mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <h1 className="text-2xl tracking-tight text-white">Akun</h1>
-        </div>
-      </div>
 
-      <main className="flex">
-        {/* Sidebar */}
-        <div className="drawer lg:drawer-open">
-          <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-          <div className="drawer-content flex flex-col items-center justify-center">
-            {/* Page content here */}
+      {/* Main Container */}
+      <div className="flex justify-center items-center h-screen">
+        <div className="relative bg-white rounded-lg overflow-hidden shadow-md flex flex-col w-3/4 border border-pinkTone">
+          <div className="bg-pinkTone text-white p-4 flex items-center justify-center rounded-t-lg">
+            <h1 className="text-2xl tracking-tight">Akun</h1>
           </div>
-          <div className="drawer-side">
-            <label
-              htmlFor="my-drawer-2"
-              aria-label="close sidebar"
-              className="drawer-overlay"
-            ></label>
-            <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content my-ul">
-              {/* Sidebar content here */}
-              <li className="mt-5">
+          <div className="flex">
+            <ul className="col-span-1 p-4">
+              {/* Konten Sidebar */}
+              <li
+                style={{ marginTop: "2rem" }}
+                className="text-1xl flex items-center justify-between"
+              >
                 <Link to="/profile">
-                  <IoPencilSharp className="text-pinkTone" />
-                  Profil Saya
+                  <div className="flex items-center">
+                    <IoPencilSharp className="text-pinkTone mr-2" />
+                    <span>Profil Saya</span>
+                  </div>
                 </Link>
               </li>
-              <li className="mt-5">
+              <li
+                style={{ marginTop: "2rem" }}
+                className="text-1xl flex items-center justify-between"
+              >
                 <Link to="/changepassword">
-                  <IoSettingsOutline className="text-pinkTone" />
-                  Ubah Password
+                  <div className="flex items-center">
+                    <IoSettingsOutline className="text-pinkTone mr-2" />
+                    <span>Ubah Password</span>
+                  </div>
                 </Link>
               </li>
-              <li className="mt-5">
+              <li
+                style={{ marginTop: "2rem" }}
+                className="text-1xl flex items-center justify-between"
+              >
                 <Link to="/purchasehistory">
-                  <IoCartOutline className="text-pinkTone" />
-                  Riwayat Pembayaran
+                  <div className="flex items-center">
+                    <IoCartOutline className="text-pinkTone mr-2" />
+                    <span>Riwayat Pembayaran</span>
+                  </div>
                 </Link>
               </li>
-              <li className="mt-5">
+              <li
+                style={{ marginTop: "2rem" }}
+                className="text-1xl flex items-center justify-between"
+              >
                 <Link to="/logout">
-                  <IoLogOutOutline className="text-pinkTone" />
-                  Keluar
+                  <div className="flex items-center">
+                    <IoLogOutOutline className="text-pinkTone mr-2" />
+                    <span>Keluar</span>
+                  </div>
                 </Link>
               </li>
             </ul>
           </div>
-        </div>
 
-        {/* Content */}
-        <div className="container mx-auto">
-          <div className="flex border rounded-lg items-center">
-            <div className="container mx-auto mt-5">
-              <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-                <div className="text-center">
-                  <img
-                    src="/profile.png"
-                    className="w-20 mx-auto block"
-                    alt="profile"
-                  />
-                </div>
-                <form className="rounded-lg ">
-                  <InputForm
-                    label="Nama"
-                    id="nama"
-                    type="text"
-                    placeholder="Nama"
-                  />
-                  <InputForm
-                    label="Email"
-                    id="email"
-                    type="email"
-                    placeholder="Email"
-                  />
-                  <InputForm
-                    label="Telepon"
-                    id="telepon"
-                    type="text"
-                    placeholder="Nomor Telepon"
-                  />
-                  <InputForm
-                    label="Negara"
-                    id="negara"
-                    type="text"
-                    placeholder="Masukan Negara tempat tinggal"
-                  />
-                  <InputForm
-                    label="Kota"
-                    id="kota"
-                    type="text"
-                    placeholder="Masukan Kota tempat tinggal"
-                  />
-                </form>
-                <div className="text-center">
-                  <button
-                    // onClick={handleDeleteFilter}
-                    className=" text-sm rounded-3xl font-semibold leading-6 bg-darkRed text-white border-4 border-darkRed m-10"
-                  >
-                    Simpan Profil Saya
-                  </button>
-                </div>
+          {/* Content */}
+          <div className="col-span-3 p-4 flex justify-center flex-col items-start">
+            <div className="text-left mx-auto max-w-7xl">
+              <div className="text-center">
+                <img
+                  src="/profile.png"
+                  className="w-20 mx-auto block"
+                  alt="profile"
+                />
+              </div>
+
+              <form>
+                <InputForm
+                  label="Nama"
+                  id="nama"
+                  type="text"
+                  placeholder="Nama"
+                />
+                <InputForm
+                  label="Email"
+                  id="email"
+                  type="email"
+                  placeholder="Email"
+                />
+                <InputForm
+                  label="Telepon"
+                  id="telepon"
+                  type="text"
+                  placeholder="Nomor Telepon"
+                />
+                <InputForm
+                  label="Negara"
+                  id="negara"
+                  type="text"
+                  placeholder="Masukan Negara tempat tinggal"
+                />
+                <InputForm
+                  label="Kota"
+                  id="kota"
+                  type="text"
+                  placeholder="Masukan Kota tempat tinggal"
+                />
+              </form>
+
+              <div className="text-center">
+                <button
+                  // onClick={handleDeleteFilter}
+                  className="text-sm rounded-2xl font-semibold leading-6 bg-darkRed text-white border-4 border-darkRed m-10"
+                >
+                  Simpan Profil Saya
+                </button>
               </div>
             </div>
           </div>
         </div>
-      </main>
+      </div>
     </>
   );
 };
@@ -155,4 +164,4 @@ InputForm.propTypes = {
   placeholder: PropTypes.string.isRequired,
 };
 
-export default MyProfile;
+export default Account;
