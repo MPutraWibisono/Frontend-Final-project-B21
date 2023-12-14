cbee4682b5df26b663401afb3ba32aa2e92f0632
Muhammad Putra Wibisono <120109799+MPutraWibisono@users.noreply.github.com>
2023-12-14 14:19:47 +0800

    fixing file structure and add feature(#8)
    * feat:homepage 1/2
    
    * homepage:3/4
    
    * homepage
    
    * feature: Homepage 1/2
    
    * konflik fe
    
    * Sprint 1 FE
    
    * Sprint 1 FE
    
    * mulai payment
    
    * fixing file structure
    
    * fixing logo
    
    * perbaiki bug
    
    * perbaiki bug
    
    * Delete back-end directory
    
    * perbaiki bug
    
    * menambah tampilan kelola kelas dan memperbaiki tampilan
    
    * fixing bug
    
    ---------
    
    Co-authored-by: aliviakr <aliviak.r@gmail.com>

--- front-end/src/pages/DashboardAdmin/Dashboard.jsx
+++ front-end/src/pages/DashboardAdmin/Dashboard.jsx
@@ -1,72 +1,14 @@
 import { TbFilter } from "react-icons/tb";
 import { LuUsers2 } from "react-icons/lu";
 import LayoutDashboard from "../../components/LayoutDashboard";
+import tableHead from "../../data/tableHead.json";
+import tableBody from "../../data/tableBody.json";
 
 const Dashboard = () => {
-  const tableHead = [
-    "ID",
-    "Kategori",
-    "Kelas Premium",
-    "Status",
-    "Metode Pembayaran",
-    "Tanggal Bayar",
-  ];
-
-  const tableBody = [
-    {
-      idUser: "johndoe123",
-      category: "UI/UX Design",
-      premiumClass: "Belajar Web Designer dengan Figma",
-      status: "SUDAH BAYAR",
-      paymentMethod: "Credit Card",
-      date: "21 Sep, 2023 at 2:00 AM",
-    },
-    {
-      idUser: "johndoe123",
-      category: "UI/UX Design",
-      premiumClass: "Belajar Web Designer dengan Figma",
-      status: "SUDAH BAYAR",
-      paymentMethod: "Credit Card",
-      date: "21 Sep, 2023 at 2:00 AM",
-    },
-    {
-      idUser: "johndoe123",
-      category: "UI/UX Design",
-      premiumClass: "Belajar Web Designer dengan Figma",
-      status: "SUDAH BAYAR",
-      paymentMethod: "Credit Card",
-      date: "21 Sep, 2023 at 2:00 AM",
-    },
-    {
-      idUser: "johndoe123",
-      category: "UI/UX Design",
-      premiumClass: "Belajar Web Designer dengan Figma",
-      status: "SUDAH BAYAR",
-      paymentMethod: "Credit Card",
-      date: "21 Sep, 2023 at 2:00 AM",
-    },
-    {
-      idUser: "johndoe123",
-      category: "UI/UX Design",
-      premiumClass: "Belajar Web Designer dengan Figma",
-      status: "SUDAH BAYAR",
-      paymentMethod: "Credit Card",
-      date: "21 Sep, 2023 at 2:00 AM",
-    },
-    {
-      idUser: "johndoe123",
-      category: "UI/UX Design",
-      premiumClass: "Belajar Web Designer dengan Figma",
-      status: "BELUM BAYAR",
-      paymentMethod: "Credit Card",
-      date: "21 Sep, 2023 at 2:00 AM",
-    },
-  ];
-
   return (
     <LayoutDashboard>
       <div className="grid grid-cols-3 gap-6 pt-3">
-        <div className="bg-darkBlue03 flex items-center justify-start p-6 rounded-xl gap-3">
+        <div className="bg-pink flex items-center justify-start p-6 rounded-xl gap-3">
           <div className="bg-white p-3 rounded-3xl">
             <LuUsers2 className="h-10 w-10 text-darkBlue05" />
           </div>
@@ -75,7 +17,7 @@ const Dashboard = () => {
             <p className="text-xl font-semibold">Active Users</p>
           </div>
         </div>
-        <div className="bg-customEmerald01 flex items-center justify-start p-6 rounded-xl gap-3">
+        <div className="bg-pink flex items-center justify-start p-6 rounded-xl gap-3">
           <div className="bg-white p-3 rounded-3xl">
             <LuUsers2 className="h-10 w-10 text-customEmerald01" />
           </div>
@@ -84,7 +26,7 @@ const Dashboard = () => {
             <p className="text-xl font-semibold">Active Class</p>
           </div>
         </div>
-        <div className="bg-darkBlue05 flex items-center justify-start p-6 rounded-xl gap-3">
+        <div className="bg-pink flex items-center justify-start p-6 rounded-xl gap-3">
           <div className="bg-white p-3 rounded-3xl">
             <LuUsers2 className="h-10 w-10 text-darkBlue05" />
           </div>
@@ -98,13 +40,13 @@ const Dashboard = () => {
       <div className="overflow-x-auto mt-10 flex flex-col space-y-4">
         <div className="flex items-center justify-between">
           <p className="text-xl font-semibold">Status Pembayaran</p>
-          <button className="flex items-center btn btn-outline btn-primary rounded-full">
+          <button className="flex items-center btn btn-sm btn-outline btn-primary rounded-full">
             <TbFilter className="h-6 w-6 " />
             <p className="font-medium">Filter</p>
           </button>
         </div>
         <table className="table ">
-          <thead className="bg-darkBlue03/20 text-black">
+          <thead className="bg-darkMagenta/20 text-black">
             <tr>
               {tableHead.map((head, index) => (
                 <th key={index}>{head}</th>
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -10,10 +10,33 @@ export default {
         paleOrange: "#fff3ec", //paragrap & main (warna pucat dominan orange)
         pinkTone: "#ffc0ad", // button
         darkRed: "#271c19", // buttonText
-        darkRed01: "##140d0b", //
+        darkRed01: "#140d0b", //
         pink: "#e78fb3", //highlight (soft pink)
         paleRed: "#ffc0ad", // secondary
         darkMagenta: "#9656a1", //tertiary
+
+        // other colors
+        customGreen01: "#86efac",
+        customLime01: "#ecfccb",
+        customEmerald01: "#10b981",
+        darkBlue05: "#6148FF",
+        darkBlue04: "#6148FF57",
+        darkBlue03: "#489CFF",
+        darkBlue02: "#D0B7E6",
+        darkBlue01: "#E2D4F0",
+        creame05: "#AA9B87",
+        creame04: "#D4C2A8",
+        creame03: "#FFE9CA",
+        creame02: "#FFF0DC",
+        creame01: "#FFF8ED",
+        alertRed: "#FF0000",
+        alertYellow: "#F9CC00",
+        alertGreen: "#73CA5C",
+        neutral05: "#151515",
+        neutral04: "#3C3C3C",
+        neutral03: "#8A8A8A",
+        neutral02: "#D0D0D0",
+        neutral01: "#FFFFFF",
       },
     },
   },
