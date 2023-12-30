d546de940f6af03f7e86f5d87eb9f58878534814
Alvan MAzid <alvantwent@gmail.com>
2023-12-30 12:43:05 +0700

    try

--- front-end/src/pages/Profile/PurchaseHistory.jsx
+++ front-end/src/pages/Profile/PurchaseHistory.jsx
@@ -7,15 +7,15 @@ import {
   IoCartOutline,
   IoLogOutOutline,
 } from "react-icons/io5";
-import Card from "../../components/CourseCard/Card";
-import kelas from "../../data/kelas.json";
-import { useDispatch } from "react-redux";
+// import CourseCard from "../../components/CourseCard/Card";
+import CardPurchase from "../../components/CourseCard/CardPurchase";
+import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../redux/actions/authActions";
 import { useNavigate } from "react-router-dom";
-
-const filteredClasses = kelas.filter(() => {
-  return true;
-});
+import { useState, useEffect } from "react";
+import { getHistory } from "../../redux/actions/courseActions";
+import { IoIosStar } from "react-icons/io";
+import { RiBookLine, RiTimeFill } from "react-icons/ri";
 
 const Sidebar = () => {
   const dispatch = useDispatch();
@@ -66,7 +66,7 @@ const Sidebar = () => {
       </li>
       <li
         style={{ marginTop: "2rem" }}
-        className="text-1xl flex items-center justify-between border-b"
+        className="text-base sm:text-xs md:text-sm lg:text-1xl flex items-center justify-between  border-b"
       >
         <div className="flex items-center cursor-pointer" onClick={onLogout}>
           <IoLogOutOutline className="text-pinkTone mr-2" />
@@ -81,6 +81,40 @@ const Sidebar = () => {
 };
 
 const PurchaseHistory = () => {
+  const dispatch = useDispatch();
+  // const { userId } = useParams();
+  const { history } = useSelector((state) => state.course);
+
+  const [errors, setErrors] = useState({
+    isError: false,
+    message: null,
+  });
+  const [order, setOrder] = useState([]);
+
+  useEffect(() => {
+    dispatch(getHistory(setErrors, errors));
+  }, [dispatch]);
+  // useEffect(() => {
+  //   if (course.length > 0) {
+  //     setCourse(course.find((item) => item.id == courseId));
+  //   }
+  // }, [course, courseId]);
+  useEffect(() => {
+    if (history.length > 0) {
+      setOrder(history.filter((item) => item.isPaid === true));
+    }
+  }, [history]);
+
+  if (history.length === 0) {
+    return (
+      <div className="pt-20 text-center">
+        <h1 className="text-2xl font-semibold mt-5 py-2">
+          Tidak ada riwayat Pembayaran
+        </h1>
+      </div>
+    );
+  }
+
   return (
     <>
       <div className="pt-20">
@@ -112,22 +146,69 @@ const PurchaseHistory = () => {
                   </div>
                 </div>
 
-                <div className="flex flex-wrap justify-around gap-5 p-2">
-                  {filteredClasses.map((kelas, index) => (
-                    <Card
-                      key={index}
-                      image={kelas.image}
-                      title={kelas.title}
-                      rating={kelas.rating}
-                      description={kelas.description}
-                      instructor={kelas.instructor}
-                      level={kelas.level}
-                      modules={kelas.modules}
-                      duration={kelas.duration}
-                      type={kelas.type}
-                      price={kelas.price}
+                {order?.map((order) => (
+                  <CardPurchase
+                    order={order}
+                    key={order.id}
+                    image={order?.image}
+                    title={order?.category?.name}
+                    rating={order?.rating}
+                    description={order?.name}
+                    instructor={order?.author}
+                    level={order?.level}
+                    modules={order?.modul}
+                    duration={order?.durasi}
+                    orderStatus={order?.status}
+                  />
+                ))}
+                <div className=" overflow-hidden rounded-2xl bg-white text-slate-500 shadow-md shadow-slate-200 py-4">
+                  <figure>
+                    <img
+                      src={order?.image}
+                      alt="card image"
+                      className="aspect-video w-full object-cover h-[100px]"
                     />
-                  ))}
+                  </figure>
+                  <div className="px-4 py-2 sm:h-34 lg:h-36 h-36 flex flex-col content-start">
+                    <div className="flex">
+                      <h3 className="text-sm font-bold text-slate-700 w-4/5">
+                        {order.title}
+                      </h3>
+                      <div className="ms-auto flex items-center text-xs">
+                        <IoIosStar className="text-yellow-400 pe-1 min-w-fit" />
+                        {order.rating}
+                      </div>
+                    </div>
+                    <p className="text-xs font-normal text-slate-700 w-4/5 ">
+                      {order.description}
+                    </p>
+                    <p className="text-xs text-slate-400">By {order.author}</p>
+                    <div className="flex justify-between py-1 text-[10px] ">
+                      <span className="text-center sm:flex sm:text-start items-center gap-1">
+                        <RiBookLine className="text-darkGrayish w-full sm:w-fit text-center" />
+                        <p>{order.modul} </p>
+                      </span>
+                      <span className="text-center sm:flex sm:text-start items-center gap-1">
+                        <RiTimeFill className="text-darkGrayish w-full sm:w-fit text-center" />
+                        <p>{order.durasi} </p>
+                      </span>
+                    </div>
+                    <div className="mt-auto">
+                      {order.status === "PAID" ? (
+                        <div className="text-xs w-auto btn btn-xs sm:btn-sm inline-flex h-8 items-center gap-2 whitespace-nowrap rounded-full bg-pinkTone px-4 font-medium tracking-wide text-white transition duration-300 hover:bg-pink focus:bg-darkMagenta focus-visible:outline-none disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none">
+                          {order.status}
+                        </div>
+                      ) : (
+                        <Link to="/PaymentDetail ">
+                          <button className="text-xs w-auto btn btn-xs sm:btn-sm inline-flex h-8 items-center gap-2 whitespace-nowrap rounded-full bg-pinkTone px-4 font-medium tracking-wide text-white transition duration-300 hover:bg-pink focus:bg-darkMagenta focus-visible:outline-none disabled:cursor-not-allowed disabled:border-emerald-300 disabled:bg-emerald-300 disabled:shadow-none">
+                            Waiting for Payment
+                          </button>
+                        </Link>
+                      )}
+                    </div>
+                  </div>
+
+                  {/*<!-- End Basic blog card --> */}
                 </div>
               </div>
             </div>
